7d26b031602d0105206991c98d8cb5d1ce344acd
Злата <zlatasmolnikova@gmail.com>
2023-09-30 19:12:47 +0500

    задание 2

---
+++ js/functions.js
@@ -0,0 +1,11 @@
+const isShorter = (string, size) => string.length <= size;
+
+isShorter('Hello', 5)
+
+const isPalindrom = (sentence) => {
+  const original = sentence;
+  sentense.replaceAll(' ', '').toUpperCase();
+return sentence == original;
+}
+
+isPalindrom('топор');
