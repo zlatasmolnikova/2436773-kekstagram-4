fe0f9c5a076b296df1b49ad2fe782eddb621969a
Злата <zlatasmolnikova@gmail.com>
2023-09-30 19:17:44 +0500

    Задание 2

--- js/functions.js
+++ js/functions.js
@@ -1,11 +1,10 @@
 const isShorter = (string, size) => string.length <= size;
 
-isShorter('Hello', 5)
-
 const isPalindrom = (sentence) => {
   const original = sentence;
-  sentense.replaceAll(' ', '').toUpperCase();
+  sentence.replaceAll(' ', '').toUpperCase();
 return sentence == original;
 }
 
 isPalindrom('топор');
+isShorter('Hello', 5)
