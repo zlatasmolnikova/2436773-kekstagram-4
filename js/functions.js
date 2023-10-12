429760073c132105a537aad2b417c38b259e1fb5
Злата <zlatasmolnikova@gmail.com>
2023-10-12 13:09:09 +0500

    Нужно больше функций

--- js/functions.js
+++ js/functions.js
@@ -4,8 +4,27 @@ const isPalindrom = (sentence) => {
   sentence.replaceAll(' ', '').toUpperCase();
   const original = sentence;
   sentence.split('').reverse().join();
-return sentence == original;
+  if (sentence == original){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+const findNumber = (line) => {
+  let result = '';
+  line = line.toString();
+
+  for (let i = 0; i < line.length; i++){
+    const currentSymbol = parseInt(line[i], 10);
+    if(!isNaN(currentSymbol)){
+      result += currentSymbol;
+    }
+  }
+  return parseInt(result, 10);
 }
 
 isPalindrom('топор');
 isShorter('Hello', 5)
+findNumber('s!2')
