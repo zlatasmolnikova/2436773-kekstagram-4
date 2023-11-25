ad338e20f733e09bc8ae9dc3641b879e8b5ae252
Злата <zlatasmolnikova@gmail.com>
2023-11-25 15:17:44 +0500

    Отрисуй меня полностью

--- js/thumbnails.js
+++ js/thumbnails.js
@@ -4,15 +4,15 @@ const picturesList = document.querySelector('.pictures');
 const similarFragment = document.createDocumentFragment();
 
 export const generateThumbnails = (pictures) => {
-    pictures.forEach(({url, description, likes, comments}) => {
-      const pictureElement = picturesContainer.cloneNode(true);
+  pictures.forEach(({url, description, likes, comments}) => {
+    const pictureElement = picturesContainer.cloneNode(true);
 
-      pictureElement.querySelector('.picture__img').src = url;
-      pictureElement.querySelector('.picture__img').alt = description;
-      pictureElement.querySelector('.picture__likes').textContent = likes;
-      pictureElement.querySelector('.picture__comments').textContent = comments.length;
+    pictureElement.querySelector('.picture__img').src = url;
+    pictureElement.querySelector('.picture__img').alt = description;
+    pictureElement.querySelector('.picture__likes').textContent = likes;
+    pictureElement.querySelector('.picture__comments').textContent = comments.length;
 
-      similarFragment.appendChild(pictureElement);
+    similarFragment.appendChild(pictureElement);
     });
   picturesList.appendChild(similarFragment);
-  };
+};
